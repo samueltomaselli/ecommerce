62918384817344108162fb2e80df6eabd0e5ae0b
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-06-15 00:20:33 -0300

    add: zod

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 export const BackButton = ({ href, label }: { href: string; label: string }) => {
   return (
-    <Button className="font-medium w-full">
+    <Button asChild variant={"link"} className="font-medium w-full">
       <Link aria-label={label} href={href}>
         {label}
       </Link>
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,33 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { AuthCard } from "./auth-card";
+import { LoginSchema } from "@/types/login-schema";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export const LoginForm = () => {
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
   return (
     <AuthCard
       cardTitle="Welcome back!"
@@ -10,7 +35,58 @@ export const LoginForm = () => {
       backButtonLabel="Create a new account"
       showSocials
     >
-      <></>
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="samueltomasellidev@gmail.com"
+                        type="email"
+                        autoComplete="email"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="********"
+                        type="password"
+                        autoComplete="current-password"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button size={"sm"} variant={"link"} asChild>
+                <Link href="/auth/reset">Forgot yout password?</Link>
+              </Button>
+            </div>
+            <Button type="submit" className="w-full my-2">
+              {"Login"}
+            </Button>
+          </form>
+        </Form>
+      </div>
     </AuthCard>
   );
 };
